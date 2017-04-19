b7d90b9bc0b467fc0276889bbbe1b540cdcfb526
Hugo Hernani <hugo@quezmedia.com>
2017-04-19 02:52:55 -0300

    Removing Unused Migration

--- db/migrate/20170419045335_remove_back_attrs_from_fun_facts.rb
+++
@@ -1,8 +0,0 @@
-class RemoveBackAttrsFromFunFacts < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :fun_facts, :background_image_file_name, :string
-    remove_column :fun_facts, :background_image_content_type, :string
-    remove_column :fun_facts, :background_image_file_size, :integer
-    remove_column :fun_facts, :background_image_updated_at, :datetime
-  end
-end
