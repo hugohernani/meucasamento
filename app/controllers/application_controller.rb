b5e40f7c576b8c9698b7ead05169560c6b55014a
Hugo Hernani <hugo@quezmedia.com>
2017-04-21 12:26:01 -0300

    Setting Current Subdomain At Env Variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :try_setting_current_event_at_rails_admin
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, :alert => exception.message
+  end
+
+  def try_setting_current_event_at_rails_admin
+    unless ENV['tenant_name']
+      req_subdomain = request.subdomain || ''
+      ENV['tenant_name'] = (req_subdomain.present? && req_subdomain != 'www') ? req_subdomain : nil
+    end
+  end
 end
