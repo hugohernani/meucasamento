6b9dca8cb8e25d308be9750a937dca6f0e71cec1
Hugo Hernani <hugo@quezmedia.com>
2017-04-28 02:48:39 -0300

    Donation Adjustments

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,6 @@ class Donation < ApplicationRecord
   belongs_to :wedding_list, class_name: WeddingList, inverse_of: :donations, required: true
 
   accepts_nested_attributes_for :orders, reject_if: :all_blank, allow_destroy: true
+
+  default_scope ->(){ order(:price) }
 end
